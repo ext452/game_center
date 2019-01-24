a50a90fd08ce583ea665e4fcd02795cacb26fb25
ext452 <ext452@gmail.com>
2019-01-24 20:29:05 +0200

    some doc`s

--- lib/game_services.dart
+++ lib/game_services.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+///Small library to work with Apple Game Center
+///
+///
 /**
  * let GC_LOGIN = "gcLogin"
     let GC_LOGOUT = "gcLogout"
@@ -17,38 +20,36 @@ const GC_SUBMIT_ACHIV = "gcSubmitAchiv";
 const GC_SHOW_LEADERBOARD = "gcShowLeadBoard";
 const GC_SHOW_ACHIEVBOARD = "gcShowAchievBoard";
 
-class DataHolder<T> {
-  T data;
-
-  DataHolder(this.data);
-
-  getData() {
-    return data;
-  }
-
-  setData(data) {
-    this.data = data;
-  }
-}
 
 class GameServices {
   static const MethodChannel _channel = const MethodChannel('game_services');
 
+
+  ///Login into Game Center
+  ///
+  ///Return `true` if operation was successful
   static Future<bool> get gcSignIn async {
     final bool result = await _channel.invokeMethod(GC_LOGIN);
     return result;
   }
 
+  ///Show native leader board passing [leaderBoardID]
+  ///
+  ///Return `true` if operation was successful
   static Future<String> showLeadBoard(String leaderBoardID) async {
     final String result = await _channel.invokeMethod(GC_SHOW_LEADERBOARD, leaderBoardID);
     return Future.error("error");
   }
 
+  ///Just show native achievement board
   static Future<bool> showAchieventBoard() async {
     final bool result = await _channel.invokeMethod(GC_SHOW_ACHIEVBOARD);
     return result;
   }
 
+  ///Open achievement by [achivKey]
+  ///
+  ///Return `true` if operation was successful
   static Future<bool> openAchievement(String achivKey) async {
     final bool result = await _channel.invokeMethod(GC_SUBMIT_ACHIV, achivKey);
     return result;
@@ -57,13 +58,9 @@ class GameServices {
   ///Submit score to GameCenter by leadBoardId
   ///It takes [params] as Map - {[leadBoardId], [score]}
   ///
-  ///Returns `true` if operation was succesfull
+  ///Returns `true` if operation was successful
   static Future<bool> reportScore(Map<String, int> params) async {
     final bool result = await _channel.invokeMethod(GC_SUBMIT_SCORE, params);
     return result;
-  }
-
-  static submitScore(int score) async {
-    _channel.invokeMethod(GC_SUBMIT_SCORE, score);
   }
 }
